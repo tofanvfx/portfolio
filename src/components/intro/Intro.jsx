81a71b1cae4a5cebbe2eb1c7cd6d9c9a33384e78
Tofan <tofanvfx@users.noreply.github.com>
2024-02-03 16:05:31 +0530

    minor changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import './Intro.css'
-import bg from '/image.png'
-import btnImg from '/hireme.png'
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
+import "./Intro.css";
+import btnImg from "/hireme.png";
+import bg from "/image.png";
 
 const Intro = () => {
   return (
     <section id="intro">
-        <div className="introContent">
-            <span className="hello">Hello,</span>
-            <span className="introText">I'm <span className="introName">Tofan</span><br />Front-end Developer</span>
-            <p className="introPara">I'm a Front-end Developer who loves combining creativity with code <br /> to make visual magic. I'm particularly passionate about 3D and VFX,<br /> where I bring a blend of technical skills and artistic flair.</p>
-            <Link><button className="btn"><img src={btnImg} alt="" className='btnImg'/>Hire Me</button></Link>
-        </div>
-        <img src={bg} alt="" className="bg" />
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I&apos;m <span className="introName">Tofan</span>
+          <br />
+          Front-end Developer
+        </span>
+        <p className="introPara">
+          I&apos;m a Front-end Developer who loves combining creativity with code{" "}
+          <br /> to make visual magic. I&apos;m particularly passionate about 3D and
+          VFX,
+          <br /> where I bring a blend of technical skills and artistic flair.
+        </p>
+        <Link>
+          <button className="btn">
+            <img src={btnImg} alt="" className="btnImg" />
+            Hire Me
+          </button>
+        </Link>
+      </div>
+      <img src={bg} alt="" className="bg" />
     </section>
-  )
-}
+  );
+};
 
 export default Intro;
